2487bf563e63fc3d59c874d525bc63ce3333c480
lussi lukas <lukas.lussi@online.gibz.ch>
2020-05-04 12:26:11 +0200

    projekt aufgesetzt

---
+++ SkrataProject/Build/HelloWorld.d
@@ -0,0 +1,48 @@
+C:\WORKSPACE\M242\SkrataProject\Build\HelloWorld.d \
+ C:\WORKSPACE\M242\SkrataProject\Build\HelloWorld.o: \
+ C:\WORKSPACE\M242\SkrataProject\Build\HelloWorld.c \
+ c:\workspace\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\io.h \
+ c:\workspace\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\sfr_defs.h \
+ c:\workspace\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\inttypes.h \
+ c:\workspace\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\workspace\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\stdint.h \
+ c:\workspace\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\iom32.h \
+ c:\workspace\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\portpins.h \
+ c:\workspace\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\common.h \
+ c:\workspace\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\version.h \
+ c:\workspace\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\fuse.h \
+ c:\workspace\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\lock.h \
+ C:\WORKSPACE\M242\AVR\ToolChain\AvrLib\include/avrlib.h \
+ c:\workspace\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\util\delay.h \
+ c:\workspace\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\util\delay_basic.h \
+ c:\workspace\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\math.h
+
+c:\workspace\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\io.h:
+
+c:\workspace\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\sfr_defs.h:
+
+c:\workspace\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\inttypes.h:
+
+c:\workspace\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\workspace\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\stdint.h:
+
+c:\workspace\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\iom32.h:
+
+c:\workspace\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\portpins.h:
+
+c:\workspace\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\common.h:
+
+c:\workspace\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\version.h:
+
+c:\workspace\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\fuse.h:
+
+c:\workspace\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\lock.h:
+
+C:\WORKSPACE\M242\AVR\ToolChain\AvrLib\include/avrlib.h:
+
+c:\workspace\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\util\delay.h:
+
+c:\workspace\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\util\delay_basic.h:
+
+c:\workspace\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\math.h:
